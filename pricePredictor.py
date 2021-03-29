c689597e237b75811503d5c7408509e51cb47a33
DaVinciTachyon <davincitachyon@gmail.com>
2021-03-29 20:37:44 +0100

    kNN single point model

--- pricePredictor.py
+++ pricePredictor.py
@@ -39,6 +39,11 @@ def greaterThanOrEqualDate(dateA, dateB):
     dtB = getDateTime(dateB)
     return dtA >= dtB
 
+def equalDate(dateA, dateB):
+    dtA = getDateTime(dateA)
+    dtB = getDateTime(dateB)
+    return dtA == dtB
+
 def getDisplaySeries(prices, sentiment):
     if(greaterThanDate(sentiment[0]['date'], prices[0]['date'])):
         newPrices = []
@@ -102,10 +107,71 @@ def displayGraphs(prices, sentiment, pricesColumns, sentimentColumns):
                 format='jpeg',
                 dpi=100,
                 bbox_inches='tight')
+    
+def getInOutSeries(prices, sentiment, pricesColumns, sentimentColumns):
+    inp = []
+    outp = []
+    i = 0
+    for s in sentiment:
+        date = s['date']
+        entry = []
+        for sC in sentimentColumns:
+            entry.append(s[sC])
+        pEntry = {}
+        for p in range(i, len(prices)):
+            if(pricesColumns[0] in pEntry and greaterThanDate(prices[p]['date'], date)):
+                for pC in pricesColumns:
+                    entry.append(pEntry[pC])
+                outp.append(prices[p]['close'] > prices[p - 1]['close'])
+                break
+            elif(equalDate(prices[p]['date'], date)):
+                for pC in pricesColumns:
+                    entry.append(prices[p][pC])
+                outp.append(prices[p + 1]['close'] > prices[p]['close'])
+                break
+            for pC in pricesColumns:
+                pEntry[pC] = prices[p][pC]
+            i += 1
+        inp.append(entry)
+    return inp, outp
+                
+def singlePointPredictor(prices, sentiment, pricesColumns, sentimentColumns):
+    prices, sentiment = getDisplaySeries(prices, sentiment)
+    X, y = getInOutSeries(prices, sentiment, pricesColumns, sentimentColumns)
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.model_selection import KFold
+    from sklearn.metrics import accuracy_score
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn.utils import shuffle
+    X = np.array(X)
+    y = np.array(y)
+    X, y = shuffle(X, y)
+    K = range(1, 10)
+    highestAccuracy = 0
+    highestAccuracyK = None
+    for k in K:
+        kf = KFold(n_splits=2)
+        accuracies = []
+        for train, test in kf.split(X):
+            model = KNeighborsClassifier(n_neighbors=k).fit(X[train], y[train])
+            pY = model.predict(X[test])
+            accuracies.append(accuracy_score(y[test], pY))
+        accuracies = np.array(accuracies)
+        accuracy = np.mean(accuracies)
+        if(accuracy > highestAccuracy):
+            highestAccuracy = accuracy
+            highestAccuracyK = k
+    
+    model = KNeighborsClassifier(n_neighbors=highestAccuracyK).fit(X, y)
+    return model, highestAccuracy
 
 sentiment = getArticleSentimentByDate()
 prices = getPrices()
 
 # getPricesExcel()
 # VAR()
-displayGraphs(prices, sentiment, ['close'], ['negativeSentiment', 'positiveSentiment'])
+# displayGraphs(prices, sentiment, ['close'], ['negativeSentiment', 'positiveSentiment'])
+sModel, sAccuracy = singlePointPredictor(prices, sentiment, ['close'], ['negativeSentiment', 'positiveSentiment'])
+print(sAccuracy)
+# tModel, tAccuracy = timeSeriesPredictor(prices, sentiment, ['close'], ['negativeSentiment', 'positiveSentiment'])
